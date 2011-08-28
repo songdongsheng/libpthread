6abde242321373b34f3c2274f08742d6dd2ac1ae
Dongsheng Song <songdongsheng@live.cn>
2011-08-28 21:56:20 +0800

    Document pthread_mutex_* functions

--- include/pthread.h
+++ include/pthread.h
@@ -79,6 +79,15 @@ typedef void    *pthread_rwlock_t;
 typedef void    *pthread_barrier_t;
 typedef void    *pthread_barrierattr_t;
 
+int pthread_attr_init(pthread_attr_t *attr);
+int pthread_attr_destroy(pthread_attr_t *attr);
+int pthread_attr_setdetachstate(pthread_attr_t *attr, int flag);
+int pthread_attr_getdetachstate(const pthread_attr_t *attr, int *flag);
+int pthread_attr_setinheritsched(pthread_attr_t *attr, int flag);
+int pthread_attr_getinheritsched(const pthread_attr_t *attr, int *flag);
+int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *size);
+int pthread_attr_setstacksize(pthread_attr_t *attr, size_t size);
+
 int pthread_create(pthread_t *t, const pthread_attr_t *attr, void *(* start_routine)(void *), void *arg);
 int pthread_join(pthread_t t, void **value_ptr);
 int pthread_detach(pthread_t t);
@@ -91,15 +100,59 @@ int pthread_kill(pthread_t t, int sig);
 void pthread_cleanup_push(void (*cleanup_routine)(void *), void *arg);
 void pthread_cleanup_pop(int execute);
 
+int pthread_cancel(pthread_t t);
+int pthread_setcancelstate(int state, int *oldstate);
+int pthread_setcanceltype(int type, int *oldtype);
+void pthread_testcancel(void);
+
 int pthread_key_create(pthread_key_t *key, void (* destructor)(void *));
 int pthread_key_delete(pthread_key_t key);
 void *pthread_getspecific(pthread_key_t key);
 int pthread_setspecific(pthread_key_t key, const void *pointer);
 
-int pthread_cancel(pthread_t t);
-int pthread_setcancelstate(int state, int *oldstate);
-int pthread_setcanceltype(int type, int *oldtype);
-void pthread_testcancel(void);
+int pthread_spin_init(pthread_spinlock_t *lock, int pshared);
+int pthread_spin_lock(pthread_spinlock_t *lock);
+int pthread_spin_trylock(pthread_spinlock_t *lock);
+int pthread_spin_unlock(pthread_spinlock_t *lock);
+int pthread_spin_destroy(pthread_spinlock_t *lock);
+
+int pthread_mutexattr_init(pthread_mutexattr_t *a);
+int pthread_mutexattr_destroy(pthread_mutexattr_t *a);
+int pthread_mutexattr_gettype(const pthread_mutexattr_t *a, int *type);
+int pthread_mutexattr_settype(pthread_mutexattr_t *a, int type);
+int pthread_mutexattr_getpshared(const pthread_mutexattr_t *a, int *pshared);
+int pthread_mutexattr_setpshared(pthread_mutexattr_t * a, int pshared);
+int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *a, int *protocol);
+int pthread_mutexattr_setprotocol(pthread_mutexattr_t *a, int protocol);
+int pthread_mutexattr_getprioceiling(const pthread_mutexattr_t *a, int *prio);
+int pthread_mutexattr_setprioceiling(pthread_mutexattr_t *a, int prio);
+
+int pthread_mutex_init(pthread_mutex_t *m, const pthread_mutexattr_t *a);
+int pthread_mutex_destroy(pthread_mutex_t *m);
+int pthread_mutex_lock(pthread_mutex_t *m);
+int pthread_mutex_trylock(pthread_mutex_t *m);
+int pthread_mutex_unlock(pthread_mutex_t *m);
+
+int pthread_barrierattr_init(void **attr);
+int pthread_barrierattr_destroy(void **attr);
+int pthread_barrierattr_setpshared(void **attr, int s);
+int pthread_barrierattr_getpshared(void **attr, int *s);
+
+int pthread_barrier_destroy(pthread_barrier_t *b);
+int pthread_barrier_init(pthread_barrier_t *b, const void *attr, unsigned int count);
+int pthread_barrier_wait(pthread_barrier_t *b);
+
+int pthread_condattr_destroy(pthread_condattr_t *a);
+int pthread_condattr_init(pthread_condattr_t *a);
+int pthread_condattr_getpshared(const pthread_condattr_t *a, int *s);
+int pthread_condattr_setpshared(pthread_condattr_t *a, int s);
+
+int pthread_cond_init(pthread_cond_t *cv, const pthread_condattr_t *a);
+int pthread_cond_destroy(pthread_cond_t *cv);
+int pthread_cond_signal (pthread_cond_t *cv);
+int pthread_cond_broadcast (pthread_cond_t *cv);
+int pthread_cond_wait (pthread_cond_t *cv, pthread_mutex_t *external_mutex);
+int pthread_cond_timedwait(pthread_cond_t *cv, pthread_mutex_t *external_mutex, const struct timespec *t);
 
 int pthread_rwlock_init(pthread_rwlock_t *rwlock_, const pthread_rwlockattr_t *attr);
 int pthread_rwlock_wrlock(pthread_rwlock_t *l);
@@ -110,59 +163,6 @@ int pthread_rwlock_unlock(pthread_rwlock_t *l);
 int pthread_rwlock_tryrdlock(pthread_rwlock_t *l);
 int pthread_rwlock_trywrlock(pthread_rwlock_t *l);
 int pthread_rwlock_destroy (pthread_rwlock_t *l);
-
-int pthread_cond_init(pthread_cond_t *cv, const pthread_condattr_t *a);
-int pthread_cond_destroy(pthread_cond_t *cv);
-int pthread_cond_signal (pthread_cond_t *cv);
-int pthread_cond_broadcast (pthread_cond_t *cv);
-int pthread_cond_wait (pthread_cond_t *cv, pthread_mutex_t *external_mutex);
-int pthread_cond_timedwait(pthread_cond_t *cv, pthread_mutex_t *external_mutex, const struct timespec *t);
-
-int pthread_mutex_init(pthread_mutex_t *m, const pthread_mutexattr_t *a);
-int pthread_mutex_destroy(pthread_mutex_t *m);
-int pthread_mutex_lock(pthread_mutex_t *m);
-int pthread_mutex_trylock(pthread_mutex_t *m);
-int pthread_mutex_unlock(pthread_mutex_t *m);
-
-int pthread_barrier_destroy(pthread_barrier_t *b);
-int pthread_barrier_init(pthread_barrier_t *b, const void *attr, unsigned int count);
-int pthread_barrier_wait(pthread_barrier_t *b);
-
-int pthread_spin_init(pthread_spinlock_t *lock, int pshared);
-int pthread_spin_destroy(pthread_spinlock_t *lock);
-int pthread_spin_lock(pthread_spinlock_t *lock);
-int pthread_spin_trylock(pthread_spinlock_t *lock);
-int pthread_spin_unlock(pthread_spinlock_t *lock);
-
-int pthread_attr_init(pthread_attr_t *attr);
-int pthread_attr_destroy(pthread_attr_t *attr);
-int pthread_attr_setdetachstate(pthread_attr_t *attr, int flag);
-int pthread_attr_getdetachstate(const pthread_attr_t *attr, int *flag);
-int pthread_attr_setinheritsched(pthread_attr_t *attr, int flag);
-int pthread_attr_getinheritsched(const pthread_attr_t *attr, int *flag);
-int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *size);
-int pthread_attr_setstacksize(pthread_attr_t *attr, size_t size);
-
-int pthread_mutexattr_init(pthread_mutexattr_t *a);
-int pthread_mutexattr_destroy(pthread_mutexattr_t *a);
-int pthread_mutexattr_gettype(const pthread_mutexattr_t *a, int *type);
-int pthread_mutexattr_settype(pthread_mutexattr_t *a, int type);
-int pthread_mutexattr_getpshared(const pthread_mutexattr_t *a, int *type);
-int pthread_mutexattr_setpshared(pthread_mutexattr_t * a, int type);
-int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *a, int *type);
-int pthread_mutexattr_setprotocol(pthread_mutexattr_t *a, int type);
-int pthread_mutexattr_getprioceiling(const pthread_mutexattr_t *a, int * prio);
-int pthread_mutexattr_setprioceiling(pthread_mutexattr_t *a, int prio);
-
-int pthread_condattr_destroy(pthread_condattr_t *a);
-int pthread_condattr_init(pthread_condattr_t *a);
-int pthread_condattr_getpshared(const pthread_condattr_t *a, int *s);
-int pthread_condattr_setpshared(pthread_condattr_t *a, int s);
-
-int pthread_barrierattr_init(void **attr);
-int pthread_barrierattr_destroy(void **attr);
-int pthread_barrierattr_setpshared(void **attr, int s);
-int pthread_barrierattr_getpshared(void **attr, int *s);
 
 #ifdef __cplusplus
 }
--- src/mutex.c
+++ src/mutex.c
@@ -32,59 +32,221 @@
 
 extern HANDLE libpthread_heap;
 
-static void arch_mutex_init(pthread_mutex_t *m)
+/**
+ * Create a mutex attribute object.
+ * @param a The pointer of the mutex attribute object.
+ * @return Always return 0.
+ * @remark We provide pthread_mutexattr_* functions only for compatibility,
+ *         please use pthread_mutex_init(&mutex, NULL) for new applications.
+ */
+int pthread_mutexattr_init(pthread_mutexattr_t *a)
 {
-    arch_thread_mutex *pv = HeapAlloc(libpthread_heap, HEAP_ZERO_MEMORY | HEAP_GENERATE_EXCEPTIONS, sizeof(arch_thread_mutex));
-    InitializeCriticalSection(& pv->mutex);
+    *a = 0;
+    return 0;
+}
 
+/**
+ * Get the mutex type attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param type The mutex type.
+ * @return Always return 0.
+ * @remark We do not support mutex type attribute, this function does nothing.
+ */
+int pthread_mutexattr_gettype(const pthread_mutexattr_t *a, int *type)
+{
+    *type = 0;
+    return 0;
+}
+
+/**
+ * Set the mutex type attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param type The mutex type.
+ * @return Always return 0.
+ * @remark We do not support mutex type attribute, this function does nothing.
+ */
+int pthread_mutexattr_settype(pthread_mutexattr_t *a, int type)
+{
+    *a = 0;
+    return 0;
+}
+
+/**
+ * Get the mutex process-shared attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param pshared The process-shared attribute.
+ * @return Always return 0.
+ * @remark The only type we support is PTHREAD_PROCESS_PRIVATE.
+ */
+int pthread_mutexattr_getpshared(const pthread_mutexattr_t *a, int *pshared)
+{
+    *type = PTHREAD_PROCESS_PRIVATE;
+    return 0;
+}
+
+/**
+ * Set the mutex process-shared attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param pshared The process-shared attribute.
+ * @return Always return 0.
+ * @remark The only type we support is PTHREAD_PROCESS_PRIVATE.
+ */
+int pthread_mutexattr_setpshared(pthread_mutexattr_t * a, int pshared)
+{
+    if (type != PTHREAD_PROCESS_PRIVATE)
+        return set_errno(EINVAL);
+
+    *a = 0;
+    return 0;
+}
+
+/**
+ * Get the mutex protocol attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param protocol The mutex protocol.
+ * @return Always return 0.
+ * @remark We do not support mutex protocol attribute, this function does nothing.
+ */
+int pthread_mutexattr_getprotocol(const pthread_mutexattr_t *a, int *protocol)
+{
+    *protocol = 0;
+    return 0;
+}
+
+/**
+ * Set the mutex protocol attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param protocol The mutex protocol.
+ * @return Always return 0.
+ * @remark We do not support mutex protocol attribute, this function does nothing.
+ */
+int pthread_mutexattr_setprotocol(pthread_mutexattr_t *a, int protocol)
+{
+    *a = 0;
+    return 0;
+}
+
+/**
+ * Get the mutex prioceiling attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param prio The mutex prioceiling attribute.
+ * @return Always return 0.
+ * @remark We do not support mutex prioceiling attribute, this function does nothing.
+ */
+int pthread_mutexattr_getprioceiling(const pthread_mutexattr_t *a, int *prio)
+{
+    *prio = 0;
+    return 0;
+}
+
+/**
+ * Set the mutex prioceiling attribute.
+ * @param a The pointer of the mutex attribute object.
+ * @param prio The mutex prioceiling attribute.
+ * @return Always return 0.
+ * @remark We do not support mutex prioceiling attribute, this function does nothing.
+ */
+int pthread_mutexattr_setprioceiling(pthread_mutexattr_t *a, int prio)
+{
+    *a = 0;
+    return 0;
+}
+
+/**
+ * Destroy a mutex attribute object.
+ * @param a The pointer of the mutex attribute object.
+ * @return Always return 0.
+ * @remark We do not support mutex attribute, all pthread_mutexattr_* functions does nothing.
+ */
+int pthread_mutexattr_destroy(pthread_mutexattr_t *a)
+{
+    *a = 0;
+    return 0;
+}
+
+static int arch_mutex_init(pthread_mutex_t *m)
+{
+    arch_thread_mutex *pv = HeapAlloc(libpthread_heap, HEAP_ZERO_MEMORY, sizeof(arch_thread_mutex));
+    if (pv == NULL)
+        return set_errno(ENOMEM);
+
+    InitializeCriticalSection(& pv->mutex);
     if (atomic_cmpxchg_ptr(m, pv, NULL) != NULL) {
         DeleteCriticalSection(& pv->mutex);
         HeapFree(libpthread_heap, 0, pv);
     }
+
+    return 0;
 }
 
+/**
+ * Create a mutex object.
+ * @param m The pointer of the mutex object.
+ * @param a The pointer of the mutex attribute object.
+ * @return If the function succeeds, the return value is 0.
+ *         If the function fails, the return value is -1,
+ *         with errno set to indicate the error (ENOMEM).
+ * @remark We provide pthread_mutexattr_* functions only for compatibility,
+ *         please use pthread_mutex_init(&mutex, NULL) for new applications.
+ */
 int pthread_mutex_init(pthread_mutex_t *m, const pthread_mutexattr_t *a)
 {
-    arch_mutex_init(m);
-    return 0;
+    return arch_mutex_init(m);
 }
 
-int pthread_mutex_destroy(pthread_mutex_t *m)
-{
-    arch_thread_mutex *pv = (arch_thread_mutex *) *m;
-    if (pv != NULL) {
-        DeleteCriticalSection(& pv->mutex);
-        HeapFree(libpthread_heap, 0, pv);
-    }
-
-    return 0;
-}
-
+/**
+ * Acquire a mutex lock.
+ * @param m The pointer of the mutex object.
+ * @return If the function succeeds, the return value is 0.
+ *         If the function fails, the return value is -1,
+ *         with errno set to indicate the error (ENOMEM).
+ */
 int pthread_mutex_lock(pthread_mutex_t *m)
 {
+    int rc;
     arch_thread_mutex *pv;
 
-    if (*m == NULL) arch_mutex_init(m);
+    if (*m == NULL) {
+        rc = arch_mutex_init(m);
+        if (rc != 0) return rc;
+    }
 
     pv = (arch_thread_mutex *) *m;
-
     EnterCriticalSection(& pv->mutex);
+
     return 0;
 }
 
+/**
+ * Try acquire a mutex lock.
+ * @param m The pointer of the mutex object.
+ * @return If the function succeeds, the return value is 0.
+ *         If the function fails, the return value is -1,
+ *         with errno set to indicate the error (ENOMEM or EBUSY).
+ */
 int pthread_mutex_trylock(pthread_mutex_t *m)
 {
     arch_thread_mutex *pv;
 
-    if (*m == NULL) arch_mutex_init(m);
+    if (*m == NULL) {
+        rc = arch_mutex_init(m);
+        if (rc != 0) return rc;
+    }
 
     pv = (arch_thread_mutex *) *m;
-
     if( 0 != TryEnterCriticalSection(& pv->mutex))
         return 0;
+
     return set_errno(EBUSY);
 }
 
+/**
+ * Release a mutex lock.
+ * @param m The pointer of the mutex object.
+ * @return If the function succeeds, the return value is 0.
+ *         If the function fails, the return value is -1,
+ *         with errno set to indicate the error (EINVAL).
+ */
 int pthread_mutex_unlock(pthread_mutex_t *m)
 {
     arch_thread_mutex *pv = (arch_thread_mutex *) *m;
@@ -94,4 +256,20 @@ int pthread_mutex_unlock(pthread_mutex_t *m)
     }
 
     return set_errno(EINVAL);
+}
+
+/**
+ * Destroy a mutex lock.
+ * @param m The pointer of the mutex object.
+ * @return Always return 0.
+ */
+int pthread_mutex_destroy(pthread_mutex_t *m)
+{
+    arch_thread_mutex *pv = (arch_thread_mutex *) *m;
+    if (pv != NULL) {
+        DeleteCriticalSection(& pv->mutex);
+        HeapFree(libpthread_heap, 0, pv);
+    }
+
+    return 0;
 }
