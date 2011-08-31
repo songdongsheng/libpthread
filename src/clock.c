d66c20fc584a483c8f40138f8947102601646d49
Dongsheng Song <dongsheng@apache.org>
2011-08-31 20:25:47 +0800

    Move nanosleep from clock.c to nanosleep.c

--- src/clock.c
+++ src/clock.c
@@ -144,59 +144,6 @@ int clock_gettime(clockid_t clock_id, struct timespec *tp)
 
 /**
  * Sleep for the specified time.
- * @param  request The desired amount of time to sleep.
- * @param  remain The remain amount of time to sleep.
- * @return If the function succeeds, the return value is 0.
- *         If the function fails, the return value is -1,
- *         with errno set to indicate the error.
- */
-int nanosleep(const struct timespec *request, struct timespec *remain)
-{
-    unsigned long ms, rc = 0;
-    unsigned __int64 u64, want, real;
-
-    union {
-        unsigned __int64 ns100;
-        FILETIME ft;
-    }  _start, _end;
-
-    if (request->tv_sec < 0 || request->tv_nsec < 0 || request->tv_nsec >= POW10_9) {
-        errno = EINVAL;
-        return -1;
-    }
-
-    if (remain != NULL) GetSystemTimeAsFileTime(&_start.ft);
-
-    want = u64 = request->tv_sec * POW10_3 + request->tv_nsec / POW10_6;
-    while (u64 > 0) {
-        if (u64 >= MAX_SLEEP_IN_MS) ms = MAX_SLEEP_IN_MS;
-        else ms = (unsigned long) u64;
-
-        u64 -= ms;
-        rc = SleepEx(ms, TRUE);
-    }
-
-    if (rc != 0) { /* WAIT_IO_COMPLETION */
-        if (remain != NULL) {
-            GetSystemTimeAsFileTime(&_end.ft);
-            real = (_end.ns100 - _start.ns100) / POW10_4;
-
-            if (real >= want) u64 = 0;
-            else u64 = want - real;
-
-            remain->tv_sec = u64 / POW10_3;
-            remain->tv_nsec = (long) (u64 % POW10_3) * POW10_6;
-        }
-
-        errno = EINTR;
-        return -1;
-    }
-
-    return 0;
-}
-
-/**
- * Sleep for the specified time.
  * @param  clock_id This argument should always be CLOCK_REALTIME (0).
  * @param  flags 0 for relative sleep interval, others for absolute waking up.
  * @param  request The desired sleep interval or absolute waking up time.
---
+++ src/nanosleep.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2011, Dongsheng Song <songdongsheng@live.cn>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file nanosleep.c
+ * @brief Implementation Code of POSIX Time Routines
+ */
+
+#include <stdio.h>
+#include <pthread_clock.h>
+
+#include <winsock2.h>
+
+#include "misc.h"
+
+/**
+ * Sleep for the specified time.
+ * @param  request The desired amount of time to sleep.
+ * @param  remain The remain amount of time to sleep.
+ * @return If the function succeeds, the return value is 0.
+ *         If the function fails, the return value is -1,
+ *         with errno set to indicate the error.
+ */
+int nanosleep(const struct timespec *request, struct timespec *remain)
+{
+    unsigned long ms, rc = 0;
+    unsigned __int64 u64, want, real;
+
+    union {
+        unsigned __int64 ns100;
+        FILETIME ft;
+    }  _start, _end;
+
+    if (request->tv_sec < 0 || request->tv_nsec < 0 || request->tv_nsec >= POW10_9) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (remain != NULL) GetSystemTimeAsFileTime(&_start.ft);
+
+    want = u64 = request->tv_sec * POW10_3 + request->tv_nsec / POW10_6;
+    while (u64 > 0) {
+        if (u64 >= MAX_SLEEP_IN_MS) ms = MAX_SLEEP_IN_MS;
+        else ms = (unsigned long) u64;
+
+        u64 -= ms;
+        rc = SleepEx(ms, TRUE);
+    }
+
+    if (rc != 0) { /* WAIT_IO_COMPLETION */
+        if (remain != NULL) {
+            GetSystemTimeAsFileTime(&_end.ft);
+            real = (_end.ns100 - _start.ns100) / POW10_4;
+
+            if (real >= want) u64 = 0;
+            else u64 = want - real;
+
+            remain->tv_sec = u64 / POW10_3;
+            remain->tv_nsec = (long) (u64 % POW10_3) * POW10_6;
+        }
+
+        errno = EINTR;
+        return -1;
+    }
+
+    return 0;
+}
